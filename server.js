0319e48bf616bd4b10c35e1a01ede0494b9ab708
harshilparmar <harshilparmar80@gmail.com>
2019-06-14 19:29:44 +0530

    last for heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static('./dist/photo-gallery'));
+app.use(express.static(__dirname + '/dist/photo-gallery'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join('./dist/photo-gallery/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/photo-gallery/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
